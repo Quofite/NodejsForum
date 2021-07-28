c1bdd5960e3511aafd0f4578131fbac5151942ac
Quofite <gleb.n1kit1n@yandex.ru>
2021-07-28 21:36:27 +0300

    changed smth

---
+++ smth.js
@@ -0,0 +1,6 @@
+// well, this file contains nothing but db data
+
+module.exports.host = "localhost";      // host
+module.exports.user = "root";           // username
+module.exports.pass = "Jkjkjav1206-_-"; // password
+module.exports.db = "usersvalut";       // name of the using db
