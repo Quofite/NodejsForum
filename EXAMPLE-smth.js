3a11dda96f05feb9848bd5291fae48d97dae2710
Quofite <gleb.n1kit1n@yandex.ru>
2021-07-28 17:14:41 +0300

    added example file with database data

---
+++ EXAMPLE-smth.js
@@ -0,0 +1,7 @@
+// well, this file contains nothing but db data
+// !!!! YOU MUST RENAME THIS FILE TO smth.js AND ENTER YOURS DATABASE DATA
+
+module.exports.host = "";     // host
+module.exports.user = "";     // username
+module.exports.pass = "";     // password
+module.exports.db = "";       // name of the using db
--- smth.js
+++
@@ -1,6 +0,0 @@
-// well, this file contains nothing but db data
-
-module.exports.host = "localhost";      // host
-module.exports.user = "root";           // username
-module.exports.pass = "Jkjkjav1206-_-"; // password
-module.exports.db = "usersvalut";       // name of the using db
