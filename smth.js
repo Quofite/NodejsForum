70f806dc010b17808df1bec5dcea8f75a0e68ab9
Quofite <gleb.n1kit1n@yandex.ru>
2021-08-02 22:57:08 +0300

    updated gitignore

--- smth.js
+++
@@ -1,6 +0,0 @@
-// well, this file contains nothing but db data
-
-module.exports.host = "localhost";       // host
-module.exports.user = "mysql";           // username
-module.exports.pass = "mysql";           // password
-module.exports.db = "uservalut";         // name of the using db
